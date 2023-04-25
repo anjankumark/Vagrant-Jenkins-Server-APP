e90996c97436956a301d362b503b829fbdd19e3d
anjankumark <anjankumar.kola@gmail.com>
2023-04-25 13:59:39 +0530

    Update hello.py
    testing the code

--- hello.py
+++ hello.py
@@ -6,3 +6,5 @@ print('a value:',a, 'b value:',b)
 a,b=b,a
 print('after swaping a and b')
 print('a value:',a ,'b value:',b)
+print('testing the code:')
+print('testing ompleted,no error found,looks good')

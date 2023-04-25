846b395888a08ae41c964921e2686f9b6cd00f50
anjankumark <anjankumar.kola@gmail.com>
2023-04-25 14:10:12 +0530

    Update test.py
    testing

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-a = 10
-b = 20
-print ('value of a and b before swap', a ,b)
-a,b = b,a
-print ('value of a and b after swap', a ,b)
-<<<<<<< Updated upstream
+
 #addition of two number
 x=20
 y=30
@@ -13,4 +8,4 @@ def f():
     print('this is function')
     
 f()    
->>>>>>> Stashed changes
+#>>>>>>> Stashed changes
